ba2a7ca50b985d0270015b8ef0bc479ac6c00b2b
David Benshachar <dbenshachar@gmail.com>
2026-02-20 21:28:12 -0800

    feat: initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	output: 'export',
+	images: {
+		unoptimized: true,
+	},
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+export default nextConfig
